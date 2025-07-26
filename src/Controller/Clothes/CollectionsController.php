6996bfcde3c8fe1c80370fe0e4d76fa16a49e65d
JamsJam <jamsjam971@hotmail.com>
2025-07-26 01:05:14 -0400

    feat:index collection

--- src/Controller/Clothes/CollectionsController.php
+++ src/Controller/Clothes/CollectionsController.php
@@ -2,17 +2,38 @@
 
 namespace App\Controller\Clothes;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\collections\collections;
+use Symfony\UX\Turbo\TurboBundle;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Provider\PageMetadata\PageMetadataProvider;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class CollectionsController extends AbstractController
 {
     #[Route('/clothes/collections', name: 'app_clothes_collections')]
-    public function index(): Response
+    public function index(
+        PageMetadataProvider $pageMetadata, 
+        Request $request,
+        EntityManagerInterface $entityManagerInterface,
+    ): Response
     {
+        $metaData = $pageMetadata->getPageMetada($request->attributes->get('_route'));
+        $collections = $entityManagerInterface->getRepository(Collections::class)->findAll();
+
+        $block = $this->renderBlockView('clothes/collections/turbo/index.html.twig','index',[
+            "collections" => $collections,
+            "routePath" => $metaData->getBreadcrumb()[count($metaData->getBreadcrumb()) - 2]->getRoute()
+        ]);
+
+                // $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
         return $this->render('clothes/collections/index.html.twig', [
-            'controller_name' => 'CollectionsController',
+            'metaData' => $metaData,
+            'block' => $block,
         ]);
     }
-}
+};
+
+
