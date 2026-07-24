25929c16c21fab2330b9d32779e915e67dc06448
JamsJam <jamsjam971@hotmail.com>
2026-07-24 13:58:48 +0200

    add accessoir avatar

---
+++ src/State/Users/CustomerMeProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\State\Users;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\ApiResource\Users\CustomerMe;
+use App\Entity\Users\Customers;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+/** @implements ProviderInterface<CustomerMe> */
+final readonly class CustomerMeProvider implements ProviderInterface
+{
+    public function __construct(
+        private Security $security,
+    ) {
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): CustomerMe
+    {
+        $customer = $this->security->getUser();
+        if (!$customer instanceof Customers || $customer->getId() === null) {
+            throw new AccessDeniedHttpException('Un client authentifié est requis.');
+        }
+
+        return new CustomerMe(
+            id: $customer->getId(),
+            email: (string) $customer->getEmail(),
+            roles: array_values($customer->getRoles()),
+        );
+    }
+}
