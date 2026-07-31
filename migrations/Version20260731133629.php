f51452443186456040ba0961519295320d2a0ff3
JamsJam <jamsjam971@hotmail.com>
2026-07-31 16:42:06 +0200

    avis

--- migrations/Version20260731133629.php
+++ migrations/Version20260731133629.php
@@ -25,6 +25,7 @@ final class Version20260731133629 extends AbstractMigration
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C68D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6C3568B40 FOREIGN KEY (customers_id) REFERENCES customers (id) ON DELETE CASCADE');
     }
+    
 
     public function down(Schema $schema): void
     {
